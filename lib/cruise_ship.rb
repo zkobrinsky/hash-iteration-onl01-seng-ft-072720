67ff0694359650853d21a002063b77018ceb452b
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:20:59 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,8 +1,8 @@
 # passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
 # suite_e: "Crumpet the Elf"
 # }
 
@@ -13,6 +13,6 @@ passengers.each do |suite, name|
     winner = name
   end
 end
- 
+
 winner
 end
