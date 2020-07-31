8856cb890a64637a819e3f5e6ae27298ad69de70
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:22:09 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,20 +1,18 @@
-passengers = {
-suite_a: "Amanda Presley",
-suite_b: "Seymour Hoffman",
-suite_c: "Alfred Tennyson",
-suite_d: "Charlie Chaplin",
-suite_e: "Crumpet the Elf"
-}
+# passengers = {
+# suite_a: "Amanda Presley",
+# suite_b: "Seymour Hoffman",
+# suite_c: "Alfred Tennyson",
+# suite_d: "Charlie Chaplin",
+# suite_e: "Crumpet the Elf"
+# }
 
 def select_winner(passengers)
-  # winner = ""
+  winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
 end
-puts winner
+
 winner
 end
-
-select_winner(passengers)
