e80d07451c9a0e13b60a3e39076a6fd88375e5fc
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-07-31 17:31:20 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,6 +14,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age <= 12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
   end
 end
 end
